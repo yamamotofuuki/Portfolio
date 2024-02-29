c95d28707bd03979ee832a19981a3a1e3a4bcde2
motoyama <7idainaru1@gmail.com>
2024-02-29 20:39:05 +0900

    PostgreSQL接続修正

--- src/main/java/com/diworksdev/portfolio/util/DBConnector.java
+++ src/main/java/com/diworksdev/portfolio/util/DBConnector.java
@@ -1,30 +1,39 @@
 package com.diworksdev.portfolio.util;
 
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnector {
-    
-    public Connection getConnection() throws SQLException {
-        Connection con = null;
-        
-        try {
-            String databaseUrl = System.getenv("DATABASE_URL");
-            URI uri = new URI(databaseUrl);
-            String username = uri.getUserInfo().split(":")[0];
-            String password = uri.getUserInfo().split(":")[1];
-            String dbUrl = "jdbc:postgresql://" + uri.getHost() + ":" + uri.getPort() + uri.getPath();
-            
-            con = DriverManager.getConnection(dbUrl, username, password);
-        } catch (URISyntaxException e) {
-            throw new SQLException("Invalid URI: " + e.getMessage());
-        } catch (SQLException e) {
-            throw new SQLException("Failed to connect to database: " + e.getMessage());
-        }
-        
-        return con;
-    }
+	
+	/**
+	 * JDBC ドライバー名
+	 */
+	private static String driverName = "org.postgresql.Driver";
+	
+	/**
+	 * データベース接続 URL
+	 */
+	private static String url = "jdbc:postgresql://ec2-44-211-104-233.compute-1.amazonaws.com:5432/d73aca8gtbjr00";
+	
+	/**
+	 * データベース接続ユーザ名・パスワード
+	 */
+	private static String user = "lyksuhktrvypwd";
+	private static String password = "0adabdc7e3eb1f3f8a67bf2e39f8bc8634852992fdb2c4c5966dd6f274b241cf";
+	
+	public Connection getConnection() {
+		Connection con = null;
+		
+		try {
+			Class.forName(driverName);
+			con = (Connection) DriverManager.getConnection(url,user,password);
+		}catch(ClassNotFoundException e ) {
+			e.printStackTrace();
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+		return con;
+	}
+	
 }
