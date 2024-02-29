08b267daab592d47f2d015e0adb7ef43fbe3e780
motoyama <7idainaru1@gmail.com>
2024-02-29 15:16:00 +0900

    PostgreSQL接続修正

--- src/main/java/com/diworksdev/portfolio/util/DBConnector.java
+++ src/main/java/com/diworksdev/portfolio/util/DBConnector.java
@@ -1,4 +1,7 @@
 package com.diworksdev.portfolio.util;
+
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -13,13 +16,25 @@ public class DBConnector {
 	/**
 	 * データベース接続 URL
 	 */
-	private static String url = System.getenv("JDBC_DATABASE_URL");
+	private static String url;
 	
 	/**
 	 * データベース接続ユーザ名・パスワード
 	 */
-	private static String user = System.getenv("DB_USER");
-	private static String password = System.getenv("DB_PASSWORD");
+	private static String user;
+	private static String password;
+	
+	static {
+		try {
+			// HerokuのDATABASE_URLから接続情報を取得
+			URI dbUri = new URI(System.getenv("DATABASE_URL"));
+			user = dbUri.getUserInfo().split(":")[0];
+			password = dbUri.getUserInfo().split(":")[1];
+			url = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+			}
+	}
 	
 	public Connection getConnection() {
 		Connection con = null;
