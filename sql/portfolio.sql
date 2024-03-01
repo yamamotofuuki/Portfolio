a96873ab7d5e8a1659381f6e31818cb25a43e6ee
motoyama <7idainaru1@gmail.com>
2024-03-01 10:49:05 +0900

    SQLファイル修正

--- sql/portfolio.sql
+++ sql/portfolio.sql
@@ -1,25 +1,9 @@
-set names utf8;
-set foreign_key_checks = 0;
+set client_encoding TO 'UTF8';
 
-drop database if exists portfolio;
-create database if not exists portfolio;
-
-use portfolio;
-
-drop table if exists webaccount;
-
-create table webaccount(
-id int not null primary key auto_increment,
-password varchar(255),
-user_name varchar(100),
-insert_date datetime,
-updated_date datetime
-);
-
-create table favorites (
-id int not null primary key auto_increment,
-user_id int,
-item_id int,
-insert_date datetime,
-FOREIGN KEY (user_id) REFERENCES webaccount(id)
+CREATE TABLE webaccount (
+id SERIAL PRIMARY KEY,
+password VARCHAR(255),
+user_name VARCHAR(100),
+insert_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+updated_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
